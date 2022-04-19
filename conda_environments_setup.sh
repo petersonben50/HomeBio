b737359c4096a267afb40577b58f6bbe5a260abe
Benjamin D. Peterson <petersonben50@gmail.com>
2022-04-19 13:17:08 -0500

    Added py-viz environment

--- conda_environments_setup.sh
+++ conda_environments_setup.sh
@@ -13,3 +13,14 @@ conda install -y -n bioinformatics -c bioconda cd-hit
 conda install -y -n bioinformatics -c bioconda fasttree
 conda install -y -n bioinformatics -c bioconda biopython
 conda install -y -n bioinformatics -c bioconda prodigal
+
+
+
+#### Py-viz environment set-up ####
+conda create --name py_viz python=3.7
+conda install -n py_viz matplotlib
+conda install -n py_viz pandas
+conda install -n py_viz -c bioconda gffutils
+conda install -n py_viz -c anaconda argparse
+conda install -n py_viz biopython
+conda install -n py_viz hmmer
