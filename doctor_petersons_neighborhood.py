1ed25c324aa0ae7e50541dbe380d18a002509e34
Benjamin D. Peterson <petersonben50@gmail.com>
2022-04-20 16:33:05 -0500

    Testing

--- doctor_petersons_neighborhood.py
+++ doctor_petersons_neighborhood.py
@@ -65,6 +65,9 @@ BIN_FILE = inputs.bin_file
 ORF_FASTA_ID = inputs.orf_fasta_id
 ORF_LOCATION = inputs.ORF_location
 SIZE_OF_BLOCK = inputs.size_of_block
+print(type(SIZE_OF_BLOCK))
+SIZE_OF_BLOCK = int(SIZE_OF_BLOCK)
+print(type(SIZE_OF_BLOCK))
 OUTPUT_LOCATION = inputs.outputLocation
 
 # Testing
