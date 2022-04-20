f57d9fe74fc5ab1a50b8b5b764696a5fecf6cd53
Benjamin D. Peterson <petersonben50@gmail.com>
2022-04-20 16:28:15 -0500

    Updated py_viz environment

--- conda_environments_setup.sh
+++ conda_environments_setup.sh
@@ -3,7 +3,7 @@
 #### Unset paths ####
 PYTHONPATH=''
 PERL5LIB=''
-
+conda update -n base conda
 
 #### Bioinformatics environment set-up ####
 conda create --name bioinformatics python=3.9
@@ -18,10 +18,12 @@ conda install -y -n bioinformatics -c bioconda pandas
 
 
 #### Py-viz environment set-up ####
-conda create --name py_viz python=3.7
-conda install -n py_viz matplotlib
-conda install -n py_viz pandas
-conda install -n py_viz -c bioconda gffutils
-conda install -n py_viz -c anaconda argparse
-conda install -n py_viz biopython
-conda install -n py_viz hmmer
+#conda env remove -n py_viz
+
+conda create -y --name py_viz python=3.9
+conda install -y -n py_viz pandas
+conda install -y -n py_viz -c anaconda argparse
+conda install -y -n py_viz biopython
+#conda install -y -n py_viz hmmer
+#conda install -y -n py_viz matplotlib
+#conda install -y -n py_viz -c bioconda gffutils
