e2f0346aebf1304eb709bcc37252c2ad15143f91
Benjamin D. Peterson <petersonben50@gmail.com>
2022-04-21 14:17:10 -0500

    Add environment for kofamscan

--- conda_environments_setup.sh
+++ conda_environments_setup.sh
@@ -16,10 +16,10 @@ conda install -y -n bioinformatics -c bioconda prodigal
 conda install -y -n bioinformatics -c bioconda pandas
 
 
-
-#### Py-viz environment set-up ####
+####---------------------------------####
+# Py-viz environment set-up
+####---------------------------------####
 #conda env remove -n py_viz
-
 conda create -y --name py_viz python=3.9
 conda install -y -n py_viz pandas
 conda install -y -n py_viz -c anaconda argparse
@@ -32,3 +32,30 @@ python -m ipykernel install
 #conda install -y -n py_viz hmmer
 #conda install -y -n py_viz matplotlib
 #conda install -y -n py_viz -c bioconda gffutils
+
+
+####---------------------------------####
+# kofamscan environment set up
+####---------------------------------####
+# Download the reference databases
+"""
+cd ~/references
+mkdir kofamscan_DBs
+cd kofamscan_DBs
+# download the ko list
+wget ftp://ftp.genome.jp/pub/db/kofam/ko_list.gz
+# download the hmm profiles
+wget ftp://ftp.genome.jp/pub/db/kofam/profiles.tar.gz
+# download kofamscan tool
+wget ftp://ftp.genome.jp/pub/tools/kofamscan/kofamscan.tar.gz
+# download README
+wget ftp://ftp.genome.jp/pub/tools/kofamscan/README.md
+
+gunzip ko_list.gz
+tar -xf profiles.tar.gz
+tar -xf kofam_scan-1.3.0.tar.gz
+"""
+
+conda create -y -n kofamscan hmmer parallel
+conda activate kofamscan
+conda install -y -c conda-forge ruby
