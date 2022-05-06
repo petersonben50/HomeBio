7824530d03473f5a75c3b2f5e083bdf8b46c1a0f
Benjamin D. Peterson <petersonben50@gmail.com>
2022-05-05 21:44:53 -0500

    fix?

--- fasta_manipulation/remove_fasta_seqs_using_list_of_headers.py
+++ fasta_manipulation/remove_fasta_seqs_using_list_of_headers.py
@@ -11,6 +11,7 @@
 ################################
 
 
+
 ####---------------------------------####
 # Load libraries
 ####---------------------------------####
@@ -48,4 +49,4 @@ with open(OUTPUT_FILE, "w") as outputFile, open(HEADERS_TO_REMOVE, "r") as remov
     remove = remover.read().split("\n")
     for sequence in SeqIO.parse(FASTA_FILE, 'fasta'):
         if sequence.id not in remove:
-            outputFile.write('>' + sequence.id + '\n' + sequence.seq + '\n')
+            outputFile.write('>' + str(sequence.id) + '\n' + str(sequence.seq) + '\n')
