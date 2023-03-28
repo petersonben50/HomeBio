3069cedaca1d905bda0118d0701e21e0ed468470
Benjamin D. Peterson <petersonben50@gmail.com>
2023-03-28 12:42:02 -0700

    fixed AA readout

--- bin/ABA_GID.py
+++ bin/ABA_GID.py
@@ -174,6 +174,8 @@ if input_type == "single_orf":
     os.system(cp_command)
 
 
+
+
 ######################################################
 ######################################################
 # Run HMM search
@@ -191,15 +193,17 @@ else:
     hmmer_log_file_name = working_directory + OUTPUT_PREFIX + '_HMM.txt'
     hmm_cmd = 'hmmsearch --tblout ' + hmmer_results_file_name + ' --cpu 4 --cut_tc ' + HMM + " " + concat_orf_to_use + " > " + hmmer_log_file_name 
     os.system(hmm_cmd)
-    # Extract amino acid sequences of all hits to each HMMs
+    # Check for hits to HMM
     hmmer_results_file_length = subprocess.check_output('wc -l < ' + hmmer_results_file_name, shell=True)
     if int(hmmer_results_file_length) > 13:
         hmmer_output = SearchIO.read(hmmer_results_file_name, 'hmmer3-tab')
         fasta_output_for_hits = OUTPUT_LOCATION + '/' + OUTPUT_PREFIX + '.faa'
         print("Extracting AA sequences for " + OUTPUT_PREFIX)
         with open(fasta_output_for_hits, 'w') as resultFile:
-            for sampleID in hmmer_output:
-                resultFile.write('>' + str(sampleID.id) + ' ' + str(sampleID.bitscore) + '\n' + str(faadict[sampleID.id]) + '\n')
+            for seq_record in SeqIO.parse(concat_orf_to_use, "fasta"):
+                for sampleID in hmmer_output:
+                    if seq_record == seq_record:
+                        resultFile.write('>' + str(sampleID.id) + ' ' + str(sampleID.bitscore) + '\n' + str(seq_record.seq) + '\n')
     else:
         print('No hits for ' + OUTPUT_PREFIX + '. Ending the script now.')
         sys.exit()
