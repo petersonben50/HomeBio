584cb71a6ab3422e082fb900eb288d5b1b729144
Benjamin D. Peterson <petersonben50@gmail.com>
2023-05-19 14:32:03 -0700

    Added script to convert FASTA data to uppercase letters

--- bin/FM_cleanFasta.py
+++ bin/FM_cleanFasta.py
@@ -22,12 +22,13 @@ from Bio import SeqIO
 parser = argparse.ArgumentParser()
 parser.add_argument('--input')
 parser.add_argument('--output')
+parser.add_argument('--convert_to_upper', action = 'store_true')
 
 # Set variables
 inputs = parser.parse_args()
 INPUT = inputs.input
 OUTPUT = inputs.output
-
+UPPER = inputs.convert_to_upper
 
 ###########################
 # Clean up fasta file
@@ -35,4 +36,7 @@ OUTPUT = inputs.output
 with open(OUTPUT, 'w') as outputFile:
     for seq_record in SeqIO.parse(INPUT, "fasta"):
         outputFile.write('>' + seq_record.id + '\n')
-        outputFile.write(str(seq_record.seq) + '\n')
+        if UPPER == True:
+            outputFile.write(str(seq_record.seq).upper() + '\n')
+        else:
+            outputFile.write(str(seq_record.seq) + '\n')
