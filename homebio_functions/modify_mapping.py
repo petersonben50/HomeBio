732d9bede88f379c4799779b6ee781d34f49c4d1
Benjamin D. Peterson <petersonben50@gmail.com>
2023-10-13 21:03:05 -0700

    Little modifications to git-script

--- homebio_functions/modify_mapping.py
+++ homebio_functions/modify_mapping.py
@@ -42,7 +42,6 @@ def filter_bam(input_bam, output_bam, fasta_headers):
     print("Number of fasta headers: " + str(len(fasta_headers)))
 
     # Set up counting variables
-    total_read_count = 0
     filtered_read_count = 0
 
     # Open input and output BAM files
@@ -51,14 +50,13 @@ def filter_bam(input_bam, output_bam, fasta_headers):
 
     # Convert fasta_headers to set for faster look-up
     fasta_headers_set = set(fasta_headers)
-    
+
     # Intermediate list to hold reads for batch write
     buffered_reads = []
 
-    # Process each targeted contig
+    # Pull out reads from each targeted contig and write to output BAM file. Use buffered write for speed.
     for fasta_header in fasta_headers_set:
         for read in input_bam_file.fetch(fasta_header):
-            total_read_count += 1
             if read.reference_name in fasta_headers_set:
                 buffered_reads.append(read)
                 filtered_read_count += 1
@@ -77,24 +75,8 @@ def filter_bam(input_bam, output_bam, fasta_headers):
     output_bam_file.close()
 
     # Print read counts
-    print(f'Number of originally mapped reads: {total_read_count}')
     print(f'Number of reads that mapped to the contigs above the size cutoff: {filtered_read_count}')
     
     # Index the output bam file
     pysam.index(output_bam)
     print(f'Generated index for {output_bam}')
-
-
-'''
-
-    # Filter bam file by fasta headers
-    for read in input_bam_file:
-        total_read_count = total_read_count + 1
-        if read.reference_name in fasta_headers:
-            output_bam_file.write(read)
-            filtered_read_count = filtered_read_count + 1
-    # Close the bam files
-    input_bam_file.close()
-    output_bam_file.close()
-
-'''
